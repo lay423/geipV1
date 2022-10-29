f627e7520a51ccb5564a42a38292ff83ecdda240
junyoungs7 <rlawnsdud98@gmail.com>
2022-10-29 19:17:57 +0900

    팀 빌딩 + 추천 빌딩 수정 + 메인 이미지 추가

--- geip/src/main/java/hello/geip/service/RiotApiService.java
+++ geip/src/main/java/hello/geip/service/RiotApiService.java
@@ -14,12 +14,14 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static hello.geip.web.basic.Key.API_KEY;
+
 @Service
 @Transactional
 public class RiotApiService {
 
 //    @Value("${RiotApiKey}")
-    private String apiKey = "";
+    private String apiKey = API_KEY;
 
     @Transactional
     public MainSummonerDTO SearchSummonerName(String name) throws JsonProcessingException {
