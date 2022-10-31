18d952fe606ef0f0fe0e952a282da1e54dc8c863
daegeun <sun98415@gmail.com>
2022-11-01 00:58:47 +0900

    내전 전적 서비스 확인

--- geip/src/main/java/hello/geip/domain/Test123.java
+++ geip/src/main/java/hello/geip/domain/Test123.java
@@ -5,11 +5,9 @@ public class Test123 {
         System.out.println("123");
         System.out.println("branch KJY");
         System.out.println("456");
-<<<<<<<<< Temporary merge branch 1
         System.out.println("branch KDG");
-=========
         System.out.println("221009");
         System.out.println("ddd");
->>>>>>>>> Temporary merge branch 2
+        System.out.println("221101");
     }
 }
